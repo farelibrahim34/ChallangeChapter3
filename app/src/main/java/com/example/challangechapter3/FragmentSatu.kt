b958c85ef44c98e9c28cfd73416046466929944f
farelibrahim34 <farrelibrahim34@gmail.com>
2022-09-09 22:58:11 +0700

    update

--- app/src/main/java/com/example/challangechapter3/FragmentSatu.kt
+++ app/src/main/java/com/example/challangechapter3/FragmentSatu.kt
@@ -21,13 +21,27 @@ class FragmentSatu : Fragment() {
         return inflater.inflate(R.layout.fragment_satu, container, false)
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         var huruf = arguments?.getString("huruf")
         Toast.makeText(context, huruf.toString(), Toast.LENGTH_SHORT).show()
         when (huruf){
             "A" -> listA()
             "B" -> listB()
+            "C" -> listC()
+            "D" -> listD()
+            "E" -> listE()
+            "F" -> listF()
+            "G" -> listG()
+            "H" -> listH()
+            "I" -> listI()
+            "J" -> listJ()
+
+
+
+
             else -> Toast.makeText(context, "else di pilih", Toast.LENGTH_SHORT).show()
         }
 
@@ -41,9 +55,9 @@ class FragmentSatu : Fragment() {
     fun listA(){
 
         val listdata = arrayListOf(
-            ListData("APEL"),
-            ListData("ABADI"),
-            ListData("ADMINISTRASI"),
+            ListData("ASTROBOY"),
+            ListData("ANTMAN"),
+            ListData("AQUAMAN"),
 
             )
         val adapter = Adapter(listdata)
@@ -56,9 +70,9 @@ class FragmentSatu : Fragment() {
     fun listB(){
 
         val listdata = arrayListOf(
-            ListData("b"),
-            ListData("B"),
-            ListData("ADMINISTRASI"),
+            ListData("BATMAN"),
+            ListData("BUCKY"),
+            ListData("BUKU"),
 
             )
         val adapter = Adapter(listdata)
@@ -68,6 +82,123 @@ class FragmentSatu : Fragment() {
         recyclerView?.layoutManager = layoutManager
         recyclerView?.adapter = adapter
     }
+    fun listC(){
+
+        val listdata = arrayListOf(
+            ListData("CAPTAIN"),
+            ListData("CYBORG"),
+            ListData("CABAI"),
+
+            )
+        val adapter = Adapter(listdata)
+        val layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false)
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.halamanSatu)
+
+        recyclerView?.layoutManager = layoutManager
+        recyclerView?.adapter = adapter
+
+    }
+    fun listD(){
+        val listdata = arrayListOf(
+            ListData("DAREDEVIL"),
+            ListData("DOMPET"),
+            ListData("DASI"),
+
+            )
+        val adapter = Adapter(listdata)
+        val layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false)
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.halamanSatu)
+
+        recyclerView?.layoutManager = layoutManager
+        recyclerView?.adapter = adapter
+    }
+    fun listE(){
+        val listdata = arrayListOf(
+            ListData("EGOIS"),
+            ListData("EDITOR"),
+            ListData("EMAIL"),
+
+            )
+        val adapter = Adapter(listdata)
+        val layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false)
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.halamanSatu)
+
+        recyclerView?.layoutManager = layoutManager
+        recyclerView?.adapter = adapter
+    }
+    fun listF(){
+        val listdata = arrayListOf(
+            ListData("FALCON"),
+            ListData("FAKULTAS"),
+            ListData("FATAMORGANA"),
+
+            )
+        val adapter = Adapter(listdata)
+        val layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false)
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.halamanSatu)
+
+        recyclerView?.layoutManager = layoutManager
+        recyclerView?.adapter = adapter
+    }
+    fun listG(){
+        val listdata = arrayListOf(
+            ListData("GUNUNG"),
+            ListData("GENGSTER"),
+            ListData("GEROBAK"),
+
+            )
+        val adapter = Adapter(listdata)
+        val layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false)
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.halamanSatu)
+
+        recyclerView?.layoutManager = layoutManager
+        recyclerView?.adapter = adapter
+    }
+    fun listH(){
+        val listdata = arrayListOf(
+            ListData("HAKIKAT"),
+            ListData("HALUSINASI"),
+            ListData("HEWAN"),
+
+            )
+        val adapter = Adapter(listdata)
+        val layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false)
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.halamanSatu)
+
+        recyclerView?.layoutManager = layoutManager
+        recyclerView?.adapter = adapter
+
+    }
+    fun listI(){
+        val listdata = arrayListOf(
+            ListData("IKAN"),
+            ListData("INVASI"),
+            ListData("INDIVIDUALISME"),
+
+            )
+        val adapter = Adapter(listdata)
+        val layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false)
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.halamanSatu)
+
+        recyclerView?.layoutManager = layoutManager
+        recyclerView?.adapter = adapter
+    }
+    fun listJ(){
+        val listdata = arrayListOf(
+            ListData("JEMBATAN"),
+            ListData("JAMBU"),
+            ListData("JAMUR"),
+
+            )
+        val adapter = Adapter(listdata)
+        val layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false)
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.halamanSatu)
+
+        recyclerView?.layoutManager = layoutManager
+        recyclerView?.adapter = adapter
+    }
+
+
 
 
 }
--- app/src/main/java/com/example/challangechapter3/MainActivity.kt
+++ app/src/main/java/com/example/challangechapter3/MainActivity.kt
@@ -9,12 +9,20 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.item_btn.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
 
     }
 }
